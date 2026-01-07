035065487168ee0f4599dad1c4a397f0cf9e4aef
Mistire <mdiwithgod@gmail.com>
2026-01-07 16:38:42 +0300

    Intialize config, requirements.txt and dockerfile

--- backend/audit/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import AuditEntry
-
-class AuditEntrySerializer(serializers.ModelSerializer):
-    user_username = serializers.CharField(source='user.username', read_only=True)
-    
-    class Meta:
-        model = AuditEntry
-        fields = '__all__'
--- backend/workflows/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import WorkflowLog
-
-class WorkflowLogSerializer(serializers.ModelSerializer):
-    actor_username = serializers.CharField(source='actor.username', read_only=True)
-    
-    class Meta:
-        model = WorkflowLog
-        fields = '__all__'
